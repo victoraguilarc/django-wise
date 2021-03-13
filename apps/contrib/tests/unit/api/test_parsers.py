5b3a1b330d41b54ddab311ccce73df4be26b5836
Victor Aguilar <vicobits@gmail.com>
2021-03-13 02:35:36 -0600

    [update] pre commit

--- apps/contrib/tests/unit/api/test_parsers.py
+++ apps/contrib/tests/unit/api/test_parsers.py
@@ -2,7 +2,7 @@
 
 import io
 
-from apps.contrib.api.parsers import SNSJsonParser, PlainTextParser
+from apps.contrib.api.parsers import PlainTextParser
 
 
 class PlainTextParserTests:
@@ -14,23 +14,3 @@ class PlainTextParserTests:
         parsed_data = parser.parse(stream)
         assert isinstance(parsed_data, str)
         assert parsed_data == self.content
-
-
-class SNSJsonParserTests:
-    content = '{"status": "OK", "items": [0,1,2]}'
-
-    def test_parse(self):
-        parser = SNSJsonParser()
-        stream = io.StringIO(self.content)
-        parsed_data = parser.parse(stream)
-        assert isinstance(parsed_data, dict)
-        assert {'status', 'items'} <= set(parsed_data.keys())
-        assert isinstance(parsed_data['items'], list)
-
-    def test_parse_invalid_json(self):
-        parser = SNSJsonParser()
-        stream = io.StringIO('invalid_json')
-        parsed_data = parser.parse(stream)
-        assert isinstance(parsed_data, str)
-        assert parsed_data == ''
-
